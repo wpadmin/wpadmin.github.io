5e76ed8a75ac041a4676e31929d4b99e82333012
wpadmin <wpadmin@users.noreply.github.com>
2025-04-14 15:18:36 +0000

    deploy: 8162c6dd6c785742fd27afda8af4be89c2c05a02

---
+++ assets/js/acecf23e.f87da4f4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdev_notes=self.webpackChunkdev_notes||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
